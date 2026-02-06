e5084fe23b0e9f9d2d3809a5deba33641eb1d64d
Danial Yessimov <kkaitenu@gmail.com>
2026-02-06 18:37:55 +0700

    Fix of errors

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import task_list, TaskViewSet
+from core.views import task_list, TaskViewSet, about, task_detail # <-- Добавь
 
 # Создаем роутер для API
 router = DefaultRouter()
@@ -15,4 +15,9 @@ urlpatterns = [
 
     # Адреса для API (все пути начнутся с api/)
     path('api/', include(router.urls)),
+
+    path('about/', about, name='about'),
+
+    path('task/<int:pk>/', task_detail, name='task_detail'),
 ]
+
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Task
 from rest_framework import viewsets
 from .serializers import TaskSerializer
+from django.shortcuts import render, get_object_or_404
 
 def task_list(request):
     # 1. Получаем все задачи из базы данных
@@ -13,3 +14,10 @@ def task_list(request):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at') # Сортируем: новые сверху
     serializer_class = TaskSerializer
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'core/task_detail.html', {'task': task})
